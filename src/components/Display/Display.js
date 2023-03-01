9337e796f3a4f385884f612c6e7a417966db6468
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-03-01 10:20:33 -0500

    Add styling to noteIntro

--- src/components/Display/Display.js
+++ src/components/Display/Display.js
@@ -2,11 +2,12 @@ import React from "react";
 import './Display.css';
 import NoteIntro from '../NoteIntro/NoteIntro' 
 
-const Display = ({allNotes}) => {
+const Display = ({allNotes,handleSelection}) => {
   const allNoteSelections =  allNotes.map((note, index) => 
     <NoteIntro
       name={note}
       key={index}
+      handleSelection={handleSelection}
      />
    
   )
