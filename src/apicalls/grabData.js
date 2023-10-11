b9fe39661d55888a39a1f56dc27fb2f5bcad4542
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-10-11 13:51:48 -0400

    Add env file and variables to hide api keys

--- src/apicalls/grabData.js
+++ src/apicalls/grabData.js
@@ -1,63 +1,66 @@
 const getNoteDetails = (note, chord) => {
-  
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': '0ba55045bemsh7c87450d2a771b8p155de8jsn591dafeb3f86',
-      'X-RapidAPI-Host': 'piano-chords.p.rapidapi.com'
+      "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+      "X-RapidAPI-Host": process.env.REACT_APP_API_HOST,
+    },
+  };
+  return fetch(
+    `https://piano-chords.p.rapidapi.com/chords/${note}/${chord}`,
+    options
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error(
+        `${res.status} ${res.statusText}: Whoops! No connection. Unable to load content!`
+      );
     }
-  }
-  return fetch(`https://piano-chords.p.rapidapi.com/chords/${note}/${chord}`, options)
-  .then(res => {
-    if(res.ok) {
-     return res.json()
-    }else{
-      throw new Error(`${res.status} ${res.statusText}: Whoops! No connection. Unable to load content!`)
-    }
-  })
-
+  });
 };
 
 const getMusicNotes = (baseNote) => {
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': '0ba55045bemsh7c87450d2a771b8p155de8jsn591dafeb3f86',
-      'X-RapidAPI-Host': 'piano-chords.p.rapidapi.com'
+      "X-RapidAPI-Key": process.env.REACT_APP_CHORD_API_KEY,
+      "X-RapidAPI-Host": process.env.REACT_APP_API_HOST,
+    },
+  };
+  return fetch(
+    `https://piano-chords.p.rapidapi.com/chords/${baseNote}`,
+    options
+  ).then((res) => {
+    if (res.ok) {
+      return res.json();
+    } else {
+      throw new Error(
+        `${res.status} ${res.statusText}: Whoops! No connection. Unable to load content!`
+      );
     }
-
-  }  
-  return fetch(`https://piano-chords.p.rapidapi.com/chords/${baseNote}`, options)
-  .then(res => {
-    if(res.ok){
-     return res.json()
-    }else{
-      throw new Error( `${res.status} ${res.statusText}: Whoops! No connection. Unable to load content!`)
-    }
-  })
-  
-  
+  });
 };
 const getBaseNotes = () => {
   const options = {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'X-RapidAPI-Key': '0ba55045bemsh7c87450d2a771b8p155de8jsn591dafeb3f86',
-      'X-RapidAPI-Host': 'piano-chords.p.rapidapi.com'
+      "X-RapidAPI-Key": process.env.REACT_APP_BASE_NOTE_API_KEY,
+      "X-RapidAPI-Host": process.env.REACT_APP_API_HOST,
+    },
+  };
+
+  return fetch("https://piano-chords.p.rapidapi.com/chords", options).then(
+    (res) => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        throw new Error(
+          `${res.status} ${res.statusText}: Whoops! No connection. Unable to load content!`
+        );
+      }
     }
-  }
+  );
+};
 
-  return fetch('https://piano-chords.p.rapidapi.com/chords', options)
-	.then(res => {
-    if(res.ok) {
-      return res.json()
-    }else{
-      throw new Error(`${res.status} ${res.statusText}: Whoops! No connection. Unable to load content!`)
-    }
-  })
-	
-}
-
-export {getMusicNotes, getNoteDetails, getBaseNotes}
-
-
+export { getMusicNotes, getNoteDetails, getBaseNotes };
