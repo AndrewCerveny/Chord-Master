d6c2a1756ab59941dec9bc7cfd2f85e9669e82a4
AndrewCerveny <andrewfcerveny0628@gmail.com>
2023-10-11 21:01:34 -0400

    This will add responsive layouts for 80% of the project

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,32 @@
 import React from "react";
-import './Navbar.css';
+import "./Navbar.css";
 import { Link } from "react-router-dom";
-import logo from '../../images/pianol.png'
+import logo from "../../images/pianol.png";
+import { FaHome } from "react-icons/fa";
+import { BiSolidPiano } from "react-icons/bi";
 
 const Navbar = () => {
-  return(
-  <nav className="nav-area">
-   <Link to={'/'} className='link title-link'>
-   <div className="title-wrap">
-      <img src={logo} alt="drawn grand piano"/>
-      <h1 className="title"> Chord Master </h1> 
-   </div>
-   </Link> 
-    <section className="nav-btn-wrapper">
-       <Link to={'/'}>
-        <button className="nav-btn"> Home </button>
-       </Link> 
-       <Link to={'/piano'} className='link'>
-          <button className="nav-btn"> See Piano</button>
-       </Link>
-    </section>
-  </nav>
-  )
-}
-export default Navbar; 
+  return (
+    <nav className="nav-area">
+      <Link to={"/"} className="link title-link">
+        <div className="title-wrap">
+          <img src={logo} alt="drawn grand piano" className="logo-pic" />
+          <h1 className="title"> Chord Master </h1>
+        </div>
+      </Link>
+      <section className="nav-btn-wrapper">
+        <Link to={"/"}>
+          <button className="nav-btn">
+            <FaHome className="home-icon" />
+          </button>
+        </Link>
+        <Link to={"/piano"} className="link">
+          <button className="nav-btn">
+            <BiSolidPiano className="piano-icon" />
+          </button>
+        </Link>
+      </section>
+    </nav>
+  );
+};
+export default Navbar;
--- src/components/NoteSpec/NoteSpec.js
+++ src/components/NoteSpec/NoteSpec.js
@@ -1,97 +1,105 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import Form from "../Form/Form";
-import './NoteSpec.css'
-import {getMusicNotes} from '../../apicalls/grabData'
+import "./NoteSpec.css";
+import { getMusicNotes } from "../../apicalls/grabData";
 import { Link } from "react-router-dom";
-import ErrorPage from '../ErrorPage/ErrorPage'
-import PropTypes from 'prop-types'
+import ErrorPage from "../ErrorPage/ErrorPage";
+import PropTypes from "prop-types";
 
 class NoteSpec extends Component {
- constructor(props) {
-  super(props)
-  this.state = {
-    chordName : '',
-    error: '',
-    baseNoteI:this.props.name,
-    baseNoteD:'',
-    chordSelections:[],
-    notes: [],
-    intervals:[],
-    error:''
+  constructor(props) {
+    super(props);
+    this.state = {
+      chordName: "",
+      error: "",
+      baseNoteI: this.props.name,
+      baseNoteD: "",
+      chordSelections: [],
+      notes: [],
+      intervals: [],
+      error: "",
+    };
   }
 
-}
+  componentDidMount = () => {
+    let root;
+    const valueBase = this.state.baseNoteI.split("");
 
-componentDidMount = () => {
-  let root;
-    const valueBase = this.state.baseNoteI.split('');
-  
-    if(valueBase[1] ==='s') {
+    if (valueBase[1] === "s") {
       const baseLetter = valueBase.splice(1);
-      valueBase.push('#')
-      const sharpName = valueBase.join('');
+      valueBase.push("#");
+      const sharpName = valueBase.join("");
       const finalSharpName = sharpName.toUpperCase();
-      root = finalSharpName
-      this.setState({baseNoteD:root})
-    }else{
-    const letterString = valueBase.join('')
-      root = letterString.toUpperCase() 
-      this.setState({baseNoteD:root}) 
-  }
-  getMusicNotes(this.state.baseNoteI)
-  .then((data) => {
-    const noteFamily = data[root]
-    const chordOptions= Object.keys(noteFamily)
-    this.setState({chordSelections:chordOptions})
-  })
-  .catch((err) => this.setState({error:err.message}))
-}
+      root = finalSharpName;
+      this.setState({ baseNoteD: root });
+    } else {
+      const letterString = valueBase.join("");
+      root = letterString.toUpperCase();
+      this.setState({ baseNoteD: root });
+    }
+    getMusicNotes(this.state.baseNoteI)
+      .then((data) => {
+        const noteFamily = data[root];
+        const chordOptions = Object.keys(noteFamily);
+        this.setState({ chordSelections: chordOptions });
+      })
+      .catch((err) => this.setState({ error: err.message }));
+  };
 
+  handleSubmit = (noteInfo) => {
+    const detailedNote = noteInfo;
+    const chordName = detailedNote.name;
+    const notesInChord = detailedNote.notes;
+    const noteIntervals = detailedNote.intervals;
+    this.setState({
+      chordName: chordName,
+      notes: notesInChord,
+      intervals: noteIntervals,
+    });
+  };
 
- handleSubmit = (noteInfo) => {
-  const detailedNote = noteInfo
-  const chordName = detailedNote.name
-  const notesInChord = detailedNote.notes
-  const noteIntervals = detailedNote.intervals 
-  this.setState({chordName:chordName,notes:notesInChord,intervals:noteIntervals })
-}
+  render() {
+    const notesInKey = this.state.notes.join(", ");
+    const intervalNotes = this.state.intervals.join(", ");
+    if (this.state.error) {
+      return <ErrorPage message={this.state.error} />;
+    }
 
-render() {
-  const notesInKey = this.state.notes.join(', ')
-  const intervalNotes = this.state.intervals.join(', ')
-  if(this.state.error) {
     return (
-      <ErrorPage message={this.state.error}/>
-    )
+      <section className="card-view">
+        <h2> Root {this.state.baseNoteD}</h2>
+        <Form
+          note={this.state.baseNoteI}
+          chordSelections={this.state.chordSelections}
+          handleSubmit={this.handleSubmit}
+        />
+        <div className="display-info">
+          <div className="chosen-chord">
+            <h3> Chord Chosen </h3>
+            <h4> {this.state.chordName} </h4>
+          </div>
+          <div className="note-area">
+            <h3> Notes In Chord</h3>
+            <div className="key-display-wrap">
+              <h4>{notesInKey} </h4>
+            </div>
+          </div>
+          <div className="interval-area">
+            <h3>Chord Intervals </h3>
+            <div className="key-interval-wrap">
+              <h4>{intervalNotes}</h4>
+            </div>
+          </div>
+        </div>
+        <Link className="link-btn-wrap" to={"/piano"}>
+          <button className="piano-btn"> Piano Compare </button>
+        </Link>
+      </section>
+    );
   }
-  
-  return(
-    <section className="card-view">
-      <h2> Root {this.state.baseNoteD}</h2>
-      <Form note={this.state.baseNoteI} chordSelections={this.state.chordSelections}  handleSubmit={this.handleSubmit}/>
-      <div className="display-info">
-        <div className="chosen-chord">  
-          <h3> Chord Chosen </h3> 
-          <h4> {this.state.chordName} </h4>
-        </div>
-        <div className="note-area">
-          <h3> Notes In Chord</h3>
-          <h4>{notesInKey} </h4>
-        </div>
-        <div className="interval-area">
-          <h3>Chord Intervals </h3>
-          <h4>{intervalNotes}</h4>
-        </div>
-      </div>
-     <Link className="link-btn-wrap" to={'/piano'}> 
-      <button className="piano-btn"> Piano </button> 
-    </Link> 
-    </section>
-  )
-}
 }
 export default NoteSpec;
 
 NoteSpec.propTypes = {
-  name:PropTypes.string.isRequired,
-}
+  name: PropTypes.string.isRequired,
+};
